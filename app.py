e5fb38c7f73e77411ac4828341a17e075acdd9af
Jooah Seo <seo.jooah@gmail.com>
2021-02-14 21:57:29 -0500

    test file update

--- app.py
+++ app.py
@@ -12,10 +12,6 @@ game_played = 0
 def home():
     board = boggle_game.make_board()
     session['boardObj'] = board
-    if(session['scores']):
-        session['scores'] = session['scores']
-    else:
-        session['scores'] = []
     return render_template('index.html', board=board)
 
 @app.route('/find/<word>')
@@ -31,17 +27,13 @@ def endgame():
     game_played += 1
     score = request.json['score']
     
-    print('session scores ', session['scores'])
-    scores = session['scores']
-    high_score = max(scores)
-    print('high score is ',high_score)
-    scores.append(score)
-    session['scores'] = scores
+    top_score = session.get("topscore",0)
+    session['topscore'] = max(score,top_score)
     
     print('server session: ',session)
-    if score > high_score:
+    if score > top_score:
         answer = True
     else:
         answer = False
-    return jsonify(gameCount = game_played, scores = session['scores'], newRecord = answer)
+    return jsonify(gameCount = game_played, newRecord = answer)
 
--- test.py
+++ test.py
@@ -3,8 +3,43 @@ from app import app
 from flask import session
 from boggle import Boggle
 
-
+app.config['TESTING'] = True
 class FlaskTests(TestCase):
 
     # TODO -- write tests for every view function / feature!
+    def test_game_board(self):
+        """Test if main page loads"""
+        with app.test_client() as client:
+            res = client.get('/')
+            html = res.get_data(as_text=True)
+            
+            self.assertEqual(res.status_code,200)
+            self.assertIn('<p>Please type your word</p>',html)
 
+    def test_find_word(self):
+        """Test if a word is valid from session board"""
+        with app.test_client() as client:
+            with client.session_transaction() as sess:
+                sess['boardObj'] = [["C", "A", "T", "T", "T"], 
+                                    ["C", "A", "T", "T", "T"], 
+                                    ["C", "A", "T", "T", "T"], 
+                                    ["C", "A", "T", "T", "T"], 
+                                    ["C", "A", "T", "T", "T"]]
+        response = client.get('/find/cat')
+        self.assertEqual(response.json['result'], 'ok')
+
+    def test_score(self):
+        with app.test_client() as client:
+            with client.session_transaction() as sess:
+                sess['topscore'] = 10
+        
+        resp = client.post('/endgame', json={'score':3})
+        self.assertEqual(resp.json['newRecord'],False)
+
+    def test_new_score(self):
+        with app.test_client() as client:
+            with client.session_transaction() as sess:
+                sess['topscore'] = 3
+        
+        resp = client.post('/endgame', json={'score':10})
+        self.assertEqual(resp.json['newRecord'],True)
